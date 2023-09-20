026439b39527866cc7cca4dbd69d890594868095
Mikhail Terentev <mihateren2003@gmail.com>
2023-09-20 11:48:59 +0300

    added 2201-2220

---
+++ Прога/2/2220.py
@@ -0,0 +1,25 @@
+def f(x, y, z):
+    a, c = max(x, y, z), min(x, y, z)
+    if (x != a) and (x != c):
+        b = x
+    elif (y != a) and (y != c):
+        b = y
+    elif (z != a) and (z != c):
+        b = z
+    if "зайка" in c:
+        print(c, len(c))
+    elif "зайка" in b:
+        print(b, len(b))
+    else:
+        print(a, len(a))
+
+    
+def main():
+    x = input()
+    y = input()
+    z = input()
+    f(x, y, z)
+
+
+if __name__ == '__main__':
+    main()
