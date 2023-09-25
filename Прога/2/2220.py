951599025572323b6a6aca43108bc8578340b53f
Mikhail Terentev <mihateren2003@gmail.com>
2023-09-25 19:31:19 +0300

    fixed 2220

--- Прога/2/2220.py
+++ Прога/2/2220.py
@@ -1,24 +1,18 @@
-def f(x, y, z):
-    a, c = max(x, y, z), min(x, y, z)
-    if (x != a) or (x != c):
-        b = x
-    elif (y != a) and (y != c):
-        b = y
-    elif (z != a) and (z != c):
-        b = z
-    if "зайка" in c:
-        print(c, len(c))
-    elif "зайка" in b:
-        print(b, len(b))
-    else:
-        print(a, len(a))
-
-    
-def main():
+def f():
     x = input()
     y = input()
     z = input()
-    f(x, y, z)
+    s1, s2, s3 = sorted((x, y, z))
+    if "зайка" in s1:
+        print(s1, len(s1))
+    elif "зайка" in s2:
+        print(s2, len(s2))
+    elif "зайка" in s3:
+        print(s3, len(s3))
+
+    
+def main():
+    f()
 
 
 if __name__ == '__main__':
