c47d6c0fef123fe9d9e284ee3b1e0d65064378f8
Mikhail Terentev <mihateren2003@gmail.com>
2024-01-08 16:52:22 +0300

    add 20, fix 14

--- Inf/Exam/14.Rto16.c
+++ Inf/Exam/14.Rto16.c
@@ -3,32 +3,15 @@
 
 int main() {
     float n;
-    scanf("%f", &n);
+    int sig;
+    printf("Введите вещественное число и количество знаков после запятой у результата: ");
+    scanf("%f %d", &n, &sig);
+
     int cel = trunc(n);
-
-    int counter = 0;
-    while (n != (int)n) {
-        n *= 10;
-        counter++;
+    printf("%x.", cel);
+    for (int i = 0; i < sig; i ++) {
+        n = n - trunc(n);
+        n *= 16;
+        printf("%x", (int)trunc(n));
     }
-    int drob = (int)n - cel * pow(10, counter);
-
-    char cel16[33] = {};
-    int i = 0;
-    while (cel > 0) {
-        int ost = cel % 16;
-        if (ost <= 9) cel16[i] = '0' + ost;
-        else if (ost == 10) cel16[i] = 'A';
-        else if (ost == 11) cel16[i] = 'B';
-        else if (ost == 12) cel16[i] = 'C';
-        else if (ost == 13) cel16[i] = 'D';
-        else if (ost == 14) cel16[i] = 'E';
-        else if (ost == 15) cel16[i] = 'F';
-        i++;
-        cel /= 16;
-    }
-    for (i; i >= 0; i--) printf("%c", cel16[i]);
-    printf(".");
-
-    // ЗАДАЧА ГРОБ
 }
