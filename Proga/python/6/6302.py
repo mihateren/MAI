b2a5e00992084c3b3cab1cc14f17c794902e49b5
mihateren <mihateren2003@gmail.com>
2024-04-26 14:49:39 +0300

    fix 6302

--- Proga/python/6/6302.py
+++ Proga/python/6/6302.py
@@ -1,10 +1,9 @@
 from requests import get
 
-
 server = 'http://' + input()
 result = 0
-response = get(server).content
-while response.content != '0':
-    result += int(response.content)
-    response = get(server).content
+response = get(server)
+while response.text != '0':
+    result += int(response.text)
+    response = get(server)
 print(result)
